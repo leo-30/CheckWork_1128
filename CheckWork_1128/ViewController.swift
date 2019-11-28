e19fcee465a45a752bf812c250cfcb4651848c73
leo-30 <mio.rabbit971.snow@gmail.com>
2019-11-28 18:47:47 +0900

    解説追加

--- CheckWork_1128/ViewController.swift
+++ CheckWork_1128/ViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //ラベルの宣言
     @IBOutlet var label: UILabel!
+    //カウントするnumberの宣言
     var number: Int = 0
 
     override func viewDidLoad() {
@@ -18,9 +20,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    //以下はボタンの宣言
     @IBAction func plus() {
         number = number + 1
+        //ラベルに数字を表示する
         label.text = String(number)
+        //色を変える処理
         if number >= 5 {
             label.textColor = UIColor.red
         } else if number <= -5 {
